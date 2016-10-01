f5e2eb28e4e3ed0271e68165a2750fe6e0fad462
Didier Dubois <didier@didier-dubois.ch>
2016-10-01 18:39:35 +0200

    Fix typo

--- server.py
+++ server.py
@@ -15,13 +15,25 @@ urls = (
     '/', 'Index',
 )
 
+menus=( ('Home', '/'),
+        ('Graph', '/graph/')
+        )
+
+def css(ids, key):
+    if ids == key: return 'active'
+    return ''
+
+def menu(key):
+    return [ (row[0], row[1], css(row[0], key) ) for row in menus ]
+
+
 class Index:
     def GET(self):
-        return render.layout(view.index())
+        return render.layout(view.index(), menu('Home'))
 
 class Graph:
     def GET(self):
-        return render.layout(view.graph())
+        return render.layout(view.graph(), menu('Graph'))
 
 class Svg:
     def GET(self, param=None):
